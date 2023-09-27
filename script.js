f7ac1e6986e60d9ca8aa27278721ae44e120f5a9
RobloxScriptHub <93853611+RobloxScriptHub@users.noreply.github.com>
2023-09-27 18:46:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ function getRandomText() {
     return textArray[randomIndex];
 }
 
+// Show a message when the user bookmarks the page
+window.addEventListener('DOMContentLoaded', function () {
+    if (window.location.hash !== '#no-bookmark') {
+        window.location.hash = 'no-bookmark';
+        alert('Bookmarking is not allowed on this page.');
+    }
+});
+
 window.onload = function () {
+    // Prevent bookmarking when leaving or refreshing the page
+    window.addEventListener('beforeunload', function (e) {
+        e.preventDefault();
+        e.returnValue = '';
+    });
+
     changingText.textContent = getRandomText();
 };
