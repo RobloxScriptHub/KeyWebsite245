e580255dcab3c71e5575c777a73a6c8ca2fa44fe
RobloxScriptHub <93853611+RobloxScriptHub@users.noreply.github.com>
2023-09-28 22:33:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,23 @@
-const changingText = document.getElementById("changing-text");
-let randomString = '';
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Redirect Check</title>
+</head>
+<body>
 
-function getRandomString(length) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
+<script>
+  // Get the current URL
+  var currentURL = window.location.href;
 
-// Function to update the text content with a new random string
-function updateRandomString() {
-    randomString = getRandomString(15);
-    changingText.textContent = randomString;
-}
+  // Check if the current URL is "https://link-center.net/924271/key-for-scripts"
+  if (currentURL === "https://link-center.net/924271/key-for-scripts") {
+    // If it is, display "Open"
+    document.write("Open");
+  } else {
+    // If it's a different website, display "Please complete the checkpoints"
+    document.write("Please complete the checkpoints");
+  }
+</script>
 
-// Show a message when the user bookmarks the page
-window.addEventListener('DOMContentLoaded', function () {
-    if (window.location.hash !== '#no-bookmark') {
-        window.location.hash = 'no-bookmark';
-        alert('Bookmarking is not allowed on this page.');
-    }
-});
-
-window.onload = function () {
-    // Prevent bookmarking when leaving or refreshing the page
-    window.addEventListener('beforeunload', function (e) {
-        e.preventDefault();
-        e.returnValue = '';
-    });
-
-    // Initialize the random string and update it every 15 seconds
-    updateRandomString();
-    setInterval(updateRandomString, 15000); // 15 seconds (15,000 milliseconds)
-};
-
-// Add this code to keep the same string if the page is refreshed within 15 seconds
-window.addEventListener('load', function () {
-    setInterval(function () {
-        updateRandomString();
-    }, 15000); // 15 seconds (15,000 milliseconds)
-});
-
+</body>
+</html>
